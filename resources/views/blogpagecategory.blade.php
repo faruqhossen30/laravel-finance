155c3f1c0a019a537ca3fb0d8a0abdf7d6a3ba4b
faruqhossen30 <faruqhossen30@gmail.com>
2024-12-07 18:29:58 +0800

    category blog author added

--- resources/views/blogpagecategory.blade.php
+++ resources/views/blogpagecategory.blade.php
@@ -18,11 +18,11 @@
                 <!-- Card -->
                 <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-neutral-700 dark:hover:border-transparent dark:hover:shadow-black/40"
                     href="{{route('single.blog',$blog->slug)}}">
-                    <div class="aspect-w-16 aspect-h-11">
+                    {{-- <div class="aspect-w-16 aspect-h-11">
                         <img class="w-full object-cover rounded-xl"
                             src="{{asset('storage/' .$blog->thumbnail)}}"
                             alt="{{$blog->title}}">
-                    </div>
+                    </div> --}}
                     <div class="my-6">
                         <h3 class="text-xl font-semibold text-gray-800 dark:text-neutral-300 dark:group-hover:text-white">
                             {{$blog->title}}
@@ -33,10 +33,10 @@
                     </div>
                     <div class="mt-auto flex items-center gap-x-3">
                         <img class="size-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80"
+                            src="{{ asset('storage/' . $blog->user->thumbnail) }}"
                             alt="{{$blog->title}}">
                         <div>
-                            <h5 class="text-sm text-gray-800 dark:text-neutral-200">By Lauren Waller</h5>
+                            <h5 class="text-sm text-gray-800 dark:text-neutral-200">{{ $blog->user->name }}</h5>
                         </div>
                     </div>
                 </a>
